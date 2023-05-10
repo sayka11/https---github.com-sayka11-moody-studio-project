fd0f1da2330339514c7c9910c3b2f99ab60ee9df
sayka11 <tvaradze.suraya@mail.ru>
2023-05-11 00:31:21 +0400

    last

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import './App.scss';
 import { HomePage } from './pages/HomePage/HomePage';
-import { HomePageHero } from './pages/HomePage/components/HomePageHero';
 
 function App() {
   return (
     <div className="App">
       <HomePage />
-      <HomePageHero />
     </div>
   );
 
--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,11 +1,11 @@
 import { HomePageFeatures } from "./components/HomePageFeatures/HomePageFeatures";
-import { HomePageHero } from "./components/HomePageHero";
+
 
 export const HomePage = () => {
     return (
         <>
             <HomePageFeatures />
-            <HomePageHero />
+          
         </>
     );
 }
