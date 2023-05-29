ac1b7faf4e973a3767ec9994b128ff304f997f58
SBMasterArt <127380114+SBMasterArt@users.noreply.github.com>
2023-05-29 21:37:32 +0400

    adaptivity

--- src/Header.tsx
+++ src/Header.tsx
@@ -3,6 +3,7 @@ import contact from './images/contact icon.svg';
 import favourite from './images/favorites icon.svg';
 import order from './images/order.svg';
 import search from './images/search icon.svg';
+import burgerMenuLogo from './images/burgerMenuLogo.svg';
 
 export const Header = () => {
   return (
@@ -12,38 +13,41 @@ export const Header = () => {
           <div className="page-header">
             <h1>MOODY STUDIO</h1>
             <div className="img-header">
-              <img className="search-icon" src={search} alt="" />
-              <img className="man" src={contact} alt="" />
-              <img className="bucket" src={order} alt="" />
-              <img className="heart" src={favourite} alt="" />
+              <img className="desktop-only" src={search} alt="" />
+              <img className="mobile-only" src={contact} alt="" />
+              <img className="mobile-only" src={order} alt="" />
+              <img className="desktop-only" src={favourite} alt="" />
+              <button className="mobile-only">
+                <img className="burgerMenuLogo" src={burgerMenuLogo} alt="" />
+              </button>
             </div>
           </div>
 
           <div className="nav-bar">
             <div className="line"></div>
             <div className="hrefLinks">
-              <a href="/" className="nav">
+              <a href="/" className="desktop-only">
                 HOME
               </a>
-              <a href="/" className="nav">
+              <a href="/" className="desktop-only">
                 STORE
               </a>
-              <a href="/" className="nav">
+              <a href="/" className="desktop-only">
                 ACCOSSORIES
               </a>
-              <a href="/" className="nav">
+              <a href="/" className="desktop-only">
                 BRAND
               </a>
-              <a href="/" className="nav">
+              <a href="/" className="desktop-only">
                 PAGES
               </a>
-              <a href="/" className="nav">
+              <a href="/" className="desktop-only">
                 ABOUT US
               </a>
-              <a href="/" className="nav">
+              <a href="/" className="desktop-only">
                 NEWS
               </a>
-              <a href="/" className="nav">
+              <a href="/" className="desktop-only">
                 CONTACT US
               </a>
             </div>
