bafc3990b30a541ef64182e61e4bc8672504713b
yusifizcode <5ba0gh2@code.edu.az>
2023-06-01 12:21:21 +0400

    01.06

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,13 @@ import React from 'react';
 import './App.scss';
 import { Footer } from './Footer';
 import { Header } from './Header';
-import { HomePage } from './pages/HomePage/HomePage';
+
 
 function App() {
   return (
     <>
       <Header />
-      <HomePage />
+      
       <Footer />
     </>
   );
